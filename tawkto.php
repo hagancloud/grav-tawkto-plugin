290c5591bde43314658f3a09a5cbee209a838b37
uptimemania <49986117+uptimemania@users.noreply.github.com>
2019-04-25 21:49:34 +0800

    Update tawkto.php

--- tawkto.php
+++ tawkto.php
@@ -20,13 +20,13 @@ class tawktoPlugin extends Plugin
         if ($this->isAdmin()) {
             return;
         }
-        $siteId = trim($this->config->get('plugins.piwik.siteId'));
+        $siteId = trim($this->config->get('plugins.tawkto.siteId'));
         if ($siteId) {
             $init = "
 //<!--Start of Tawk.to Script-->
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+var s1=document.createElement('script'),s0=document.getElementsByTagName('script')[0];
 s1.async=true;
 s1.src='https://embed.tawk.to/{$siteId}/default';
 s1.charset='UTF-8';
@@ -41,4 +41,3 @@ s0.parentNode.insertBefore(s1,s0);
         }
     }
 }
-
